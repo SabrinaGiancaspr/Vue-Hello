40acea5a4436a573b9baec287552844d71f9aca4
SabrinaGiancaspro <sabrina.giancaspro13@gmail.com>
2023-10-23 18:42:35 +0200

    add img

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ createApp({
   data() {
     return {
       message: 'Madame ti amo',
+      image: 'https://media-assets.vanityfair.it/photos/6534d8df2443ab125f4cd7b6/master/w_1600%2Cc_limit/GettyImages-1749043299.jpg',
     };
   },
 }).mount('#app');
